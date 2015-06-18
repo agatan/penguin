bb026f9659a96866c863baf96c82fb0823fcaa82
agatan <m.k.mahoo@gmail.com>
2015-06-18 12:55:45 +0900

    move some parser from primitives module to combinator module

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,30 @@
 
 pub mod primitives;
 pub mod combinator;
+
+#[cfg(test)]
+mod tests {
+    use primitives::*;
+    use combinator::*;
+    use std::iter::{Iterator};
+
+    #[test]
+    fn digits_test() {
+        let non_z = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        let non_zero = set(&non_z);
+        let digit = select(exact('0'), non_zero.clone());
+        let dig_head = seq(seq(non_zero, option(digit.clone())), option(digit.clone()));
+        let dig_tail = seq(seq(seq(exact(','), digit.clone()), digit.clone()), digit);
+        let zero = success(seq(exact('0'), end()));
+        let mut digits = select(success(zero), success(seq(seq(dig_head, many(dig_tail)), end())));
+
+        for s in ["0", "1", "1,234"].iter() {
+            let src = s.chars().peekable();
+            let ret = digits.parse(src);
+            assert!(ret.is_ok());
+            if let Ok(((), mut ctx)) = ret {
+                assert!(ctx.peek().is_none());
+            }
+        }
+    }
+}
